538d3ae01b1afa4e9599aa090c4e6bf489386526
Marvin <373268232@qq.com>
2017-04-07 14:56:50 +0800

    项目初始化

---
+++ PracticeLGDemo/PracticeLGDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticeLGDemo
+//
+//  Created by Marvin on 2017/4/7.
+//  Copyright © 2017年 Marvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PracticeLGDemo/PracticeLGDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PracticeLGDemo
+//
+//  Created by Marvin on 2017/4/7.
+//  Copyright © 2017年 Marvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
